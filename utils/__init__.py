e7433cdbedc2d8f3a3e579cb51f10b2091ccc84a
Michael <145378101+MaksLol666@users.noreply.github.com>
2026-02-16 12:05:18 +0300

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+from .decorators import check_subscription, admin_only
+from .promo_setup import create_start_promos
+
+__all__ = [
+    'check_subscription',
+    'admin_only', 
+    'create_start_promos'
+]
