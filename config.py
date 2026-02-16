43d7bdc548222d9e96bb65c6bc2c56cd26222c2b
Michael <145378101+MaksLol666@users.noreply.github.com>
2026-02-16 11:28:59 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+ADMIN_IDS = [1691654877]
+ADMIN_USERNAME = "@CIM_KAPTbI_BIO"
+
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+CHANNEL_LINK = os.getenv("CHANNEL_LINK")
+CHAT_LINK = os.getenv("CHAT_LINK")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+MIN_BET = 100
+MAX_BET = 1000000
+
+BONUS_COOLDOWN = 5 * 60 * 60
+BONUS_MIN = 1000
+BONUS_MAX = 10000
+
+BOT_VERSION = "1.0"
+BOT_RELEASE_DATE = "2024-04-15"
