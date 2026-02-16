2ed8b38cd43f50d27409bbe790444a2e47dbd363
Michael <145378101+MaksLol666@users.noreply.github.com>
2026-02-16 12:01:39 +0300

    Create __init__.py

---
+++ keyboards/__init__.py
@@ -0,0 +1,22 @@
+from .inline import *
+from .roulette import get_roulette_keyboard, get_roulette_bet_keyboard
+
+__all__ = [
+    'get_start_keyboard',
+    'get_main_menu', 
+    'get_casino_menu',
+    'get_business_menu',
+    'get_back_button',
+    'get_roulette_keyboard',
+    'get_roulette_bet_keyboard',
+    'get_vip_market_keyboard',
+    'get_vip_statuses_keyboard',
+    'get_confirm_keyboard',
+    'get_inventory_keyboard',
+    'get_daily_quests_keyboard',
+    'get_blackjack_keyboard',
+    'get_crash_keyboard',
+    'get_poker_keyboard',
+    'get_poker_start_keyboard',
+    'get_poker_actions_keyboard'
+]
